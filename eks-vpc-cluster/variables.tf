01ecb71fe99c925b4d3b381cb6d6a79ce69fce54
Eugene Churylov <75759692+EugeneChurylov@users.noreply.github.com>
2025-09-13 15:56:05 +0200

    1st

--- eks-vpc-cluster/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "region" { default = "eu-central-1" }
-variable "profile" { default = "default" }
-variable "project" { default = "eks-vpc-lesson" }
--- eks-vpc-cluster/vpc/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "project" {}
-variable "cidr"    { default = "10.0.0.0/16" }
-variable "azs"     { type = list(string) }
-variable "public_subnets"  { type = list(string) }
-variable "private_subnets" { type = list(string) }
