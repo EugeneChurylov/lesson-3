006fd2a5cd20f4e672b3fcd14effcb4083e5ad37
Eugene Churylov <75759692+EugeneChurylov@users.noreply.github.com>
2025-09-14 13:00:28 +0200

    CI debug: print and run train.sh with /bin/sh -x

--- ci/train.sh
+++ ci/train.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -eu
 
+echo ">>> running train.sh"
 aws --version
-aws stepfunctions list-state-machines --region "${AWS_DEFAULT_REGION}"
+aws stepfunctions start-execution \
+  --region "${AWS_DEFAULT_REGION}" \
+  --state-machine-arn "${SFN_ARN}" \
+  --name "train-$(date +%s)" \
+  --input "{\"source\":\"gitlab-ci\",\"commit\":\"${CI_COMMIT_SHORT_SHA}\"}"
