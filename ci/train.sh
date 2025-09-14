0575fa3d26d9736078cff10bb13ec75aad352aff
Eugene Churylov <75759692+EugeneChurylov@users.noreply.github.com>
2025-09-14 12:57:50 +0200

    Fix: correct stepfunctions command

--- ci/train.sh
+++ ci/train.sh
@@ -1,14 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -eu
 
-# Діагностика оточення (не друкує секрети)
 aws --version
-aws configure list
-aws sts get-caller-identity
-
-# Пуск Step Functions
-aws stepfunctions start-execution \
-  --region "${AWS_DEFAULT_REGION}" \
-  --state-machine-arn "${SFN_ARN}" \
-  --name "train-$(date +%s)" \
-  --input "{\"source\":\"gitlab-ci\",\"commit\":\"${CI_COMMIT_SHORT_SHA}\"}"
+aws stepfunctions list-state-machines --region "${AWS_DEFAULT_REGION}"
